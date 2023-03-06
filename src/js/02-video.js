f8e767f4682c2a2493ebbc10061476cdcfede975
Yurii Okhvatenko <fess517@gmail.com>
2023-03-06 23:15:38 +0200

    task2v1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+import Player from '@vimeo/player';
+
+
+
+// console.dir(throttle);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentVideoTime = 'videoplayer-current-time';
+
+const onPlay = function(data) {
+    const videoUpdateTime = data.seconds;
+    console.log(videoUpdateTime);
+    console.log(data);
+ 
+  localStorage.setItem(currentVideoTime, videoUpdateTime)
+};
+
+player.on('timeupdate', throttle(onPlay, 2000));
+console.log(player);
+console.log(onPlay);
+
+function continueFromPausedTime() {
+  const pausedTime = localStorage.getItem(currentVideoTime)
+
+  if(pausedTime) {
+      player.setCurrentTime(pausedTime);
+  }
+}
+
+continueFromPausedTime();
