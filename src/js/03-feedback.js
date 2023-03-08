db5f2d939fff07e5eb097797e1f95a8d0c57ac06
Yurii Okhvatenko <fess517@gmail.com>
2023-03-08 23:13:27 +0200

    finish hw8v1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const localStorageValuesFromForm = {};
+
+formRef.addEventListener('input', throttle(handleInputForLocalStorage, 500));
+formRef.addEventListener('submit', handleSubmitBtnClick);
+
+handlePageReloading();
+
+
+function handleInputForLocalStorage(e) {
+    localStorageValuesFromForm[e.target.name] = e.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localStorageValuesFromForm));
+}
+
+function handleSubmitBtnClick(e) {
+    e.preventDefault();
+    formRef.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function handlePageReloading() {
+    const savingDataFromForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (savingDataFromForm) {
+        formRef.email.value = savingDataFromForm.email || '';
+        formRef.message.value = savingDataFromForm.message || '';
+    }
+    
+}
 
